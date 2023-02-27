0b41b7c7c933098b707ad43803b157afb3cbfc7e
kaliboy <kaliboy@kali>
2023-02-27 14:44:35 +0100

    super

--- mkzw/settings.py
+++ mkzw/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
 )
 MEDIA_ROOT = os.path.join(BASE_DIR, "links/static/links/images")
 STATIC_ROOT = os.path.join(BASE_DIR, "links/static/")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
